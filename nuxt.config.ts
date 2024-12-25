9d06c2638d40f1fc366879c61bd6f63b8b3f0f33
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-12-25 19:27:00 +0700

    env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   modules: ['@nuxtjs/tailwindcss', 'nuxt-auth-utils'],
 
   css: ['~/assets/style.css'],
